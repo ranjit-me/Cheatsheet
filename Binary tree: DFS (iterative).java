fb0f0c3284e6d5ba680397d4d98d19fbce47035c
Edith <144516930+ranjit-me@users.noreply.github.com>
2026-01-17 08:55:33 +0530

    Create Binary tree: DFS (iterative).java

---
+++ Binary tree: DFS (iterative).java
@@ -0,0 +1,18 @@
+public int dfs(TreeNode root) {
+    Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+    int ans = 0;
+
+    while (!stack.empty()) {
+        TreeNode node = stack.pop();
+        // do logic
+        if (node.left != null) {
+            stack.push(node.left);
+        }
+        if (node.right != null) {
+            stack.push(node.right);
+        }
+    }
+
+    return ans;
+}
